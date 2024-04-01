7882c9d143b7cbc9968f0a1098f92406899f7803
ecourse <smkang@codenuri.co.kr>
2024-04-01 10:47:23 +0900

    Add files via upload

---
+++ DAY1/6_range_for3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+struct Point3D
+{
+	int x, y, z;
+
+	int* begin() { return &x;}
+	int* end()   { return &z + 1 ;} // end 는 마지막요소의 다음 주소
+};
+// 단 위 코드는 모든 멤버가 동일한 타입이고, 
+// 연속된 메모리에 놓여 있다는 가정하에만 사용가능합니다.
+
+
+int main()
+{
+	Point3D p;
+	p.x = 10;
+	p.y = 20;
+	p.z = 30;
+
+	for (int n : p)  // 될까요 ? 되게하려면 필요한 것은 ?
+		std::cout << n << std::endl; 
+
+//	auto first = std::begin(p); // 또는 p.begin()
+//	auto last  = std::end(p);   // p.end()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
