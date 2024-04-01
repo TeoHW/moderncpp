74c2ba144da0ccf64489e0c38a3e62757a8fc123
ecourse <smkang@codenuri.co.kr>
2024-04-02 08:36:25 +0900

    Add files via upload

---
+++ DAY2/2_후위반환타입1.cpp
@@ -0,0 +1,20 @@
+﻿// 1_후위반환타입1.cpp - 교재 73 page
+
+// C/C++ 에서 전통적인 함수 모양
+/*
+int square(int a)
+{
+	return a * a;
+}
+*/
+
+// C++11 에서 추가된 "suffix return type" 모양의 함수
+auto square(int a) -> int
+{
+	return a * a;
+}
+
+int main()
+{
+	square(3);
+}
