ed0fa794cec00427071578664c798cdadf547c42
ecourse <smkang@codenuri.co.kr>
2024-04-01 15:37:34 +0900

    Add files via upload

---
+++ DAY1/18_if_init.cpp
@@ -0,0 +1,17 @@
+﻿// 18_if_init    50 page
+
+int foo() { return 100; }
+
+int main()
+{
+	int ret = foo();
+
+	if (ret == 0)
+	{
+	}
+	// C++17 부터 위 코드를 아래처럼 사용가능합니다
+	if( int ret2 = foo(); ret2 == 0)
+	{
+	} // <== 여기서 ret2 파괴
+
+}
