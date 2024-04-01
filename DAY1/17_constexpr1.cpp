8747070b020db41aff000c1a3c1c0aa9417c51c3
ecourse <smkang@codenuri.co.kr>
2024-04-01 14:39:48 +0900

    Add files via upload

---
+++ DAY1/17_constexpr1.cpp
@@ -0,0 +1,24 @@
+﻿// 16_constexpr1
+#include <array>
+
+int main()
+{
+	int n = 10;
+	const int c1 = 10; 
+	const int c2 = n;  
+
+	// 다음중 에러를 모두 고르세요
+	std::array<int, 10> a1; // ok
+	a1[0] = 10;
+
+	std::array<int, n> a2;	// error
+							// 템플릿 인자의 값은 반드시 컴파일할때
+							// 알아야 합니다.
+	//======================================
+	// 핵심은 아래 2줄
+	std::array<int, c1> a3; // ok
+	std::array<int, c2> a4; // error
+
+}
+
+
