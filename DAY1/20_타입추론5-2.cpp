7151aa5dbec341f7d29c63d5c01991c1f5f5dc58
ecourse <smkang@codenuri.co.kr>
2024-04-01 17:04:48 +0900

    Add files via upload

--- DAY1/20_타입추론5-2.cpp
+++ DAY1/20_타입추론5-2.cpp
@@ -6,4 +6,21 @@ int main()
 	// 왜 아래 같은 현상이 있을까요 ?
 	foo("apple", "banana"); // ok
 	goo("apple", "banana"); // error
+
+	// "apple"  : const char[6]
+	// "banana" : const char[7]
+	// => 위 2개의 문자열은 "다른 타입" 입니다.
+
+	foo("apple", "banana");	
+		// => 인자는 배열이지만 값타입으로 받으므로 포인터로 받게 됩니다.
+		// => const char[6] => const char*
+		// => const char[7] => const char*
+		// => 이므로 결국 2개는 같은 타입으로 전달됩니다.
+		// => 따라서 ok!!
+
+	goo("apple", "banana");	
+		// => 참조 타입(T&) 로 받게 되므로
+		// => 배열을 보내면 배열로 받게 됩니다.
+		// => "apple", "banana" 는 다른 타입인데
+		// => T a, T b 즉, 같은 타입을 요구하므로 error
 }
