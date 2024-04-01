221f4fa8694de17230f6d5adbdf5452baf188c41
ecourse <smkang@codenuri.co.kr>
2024-04-01 09:28:56 +0900

    Add files via upload

---
+++ DAY1/2_static_assert1.cpp
@@ -0,0 +1,12 @@
+﻿// 7page
+// 64bit 전용프로그램을 작성했다.
+
+// static_assert : 조건을 만족하지 않으면 컴파일을 중단하고
+//				   메세지 출력
+static_assert( sizeof(int*) == 8, "error not 64bit");
+
+int main()
+{
+	int n = 0;
+	int* p = &n;
+}
