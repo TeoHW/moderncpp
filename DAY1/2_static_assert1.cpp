eaa55ba9702942a747dfa073573ec56ac4d2599c
ecourse <smkang@codenuri.co.kr>
2024-04-01 09:31:47 +0900

    Add files via upload

--- DAY1/2_static_assert1.cpp
+++ DAY1/2_static_assert1.cpp
@@ -3,7 +3,10 @@
 
 // static_assert : 조건을 만족하지 않으면 컴파일을 중단하고
 //				   메세지 출력
-static_assert( sizeof(int*) == 8, "error not 64bit");
+// => 컴파일시에만 사용하게 되므로
+// => 메모리 증가, 실행성능에는 아무 영향이 없습니다
+// => 많이 사용할수록 좋습니다.
+static_assert( sizeof(int*) == 16, "error not 64bit");
 
 int main()
 {
