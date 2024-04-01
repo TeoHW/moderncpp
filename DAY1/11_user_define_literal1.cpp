1fa529c9d034932fc02b29fafff19292daa65cfe
ecourse <smkang@codenuri.co.kr>
2024-04-01 12:05:56 +0900

    Add files via upload

---
+++ DAY1/11_user_define_literal1.cpp
@@ -0,0 +1,25 @@
+﻿class Meter
+{
+	int value;
+public:
+	Meter(int n) : value(n) {}
+};
+
+Meter operator""m( int value )
+{
+	Meter meter(value);
+	return meter;
+}
+
+int main()
+{
+	double d = 3.4; // "3.4"  는 double
+	float f = 3.4f; // "3.4f" 는 float
+
+	auto a1 = 3m;   // 이순간 operator""m(3) 이 호출됩니다.
+//	auto a2 = 3cm;  //       operator""cm(3) 
+}
+
+
+
+
