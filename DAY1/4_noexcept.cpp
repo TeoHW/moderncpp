fe2f0f8f17d6f2270de0f57d7b11bfc0917ea357
ecourse <smkang@codenuri.co.kr>
2024-04-01 10:08:24 +0900

    Add files via upload

--- DAY1/4_noexcept.cpp
+++ DAY1/4_noexcept.cpp
@@ -6,7 +6,8 @@
 // 1. 함수가 예외가 있는지 조사할때
 // 2. 함수가 예외가 없음을 알릴때
 
-void foo() 
+//void foo()         // 이렇게 하면 예외 가능성이 있는 함수
+void foo() noexcept  // 이렇게 하면 예외 가능성이 없음을 컴파일러에게 알리는것
 {
 }
 void goo()
@@ -18,8 +19,13 @@ void goo()
 int main()
 {
 	// noexcept( 함수 호출식 ) : 함수호출시 예외 가능성이 있는지 조사
-	bool b1 = noexcept( foo() );
-	bool b2 = noexcept( goo() );
+	//						=> 실제 함수를 호출하는 것이 아니라
+	//						=> 컴파일 할때 조사
+	bool b1 = noexcept( foo() ); // false
+								 // 단, noexcept 가 붙어 있으면 true
+	bool b2 = noexcept( goo() ); // false
+
+	std::cout << b1 << ", " << b2 << std::endl;
 }
 
 
