2853330a1ec9511f495becab6d8bc541260f8ff4
ecourse <smkang@codenuri.co.kr>
2024-04-01 12:07:57 +0900

    Add files via upload

--- DAY1/11_user_define_literal1.cpp
+++ DAY1/11_user_define_literal1.cpp
@@ -5,19 +5,20 @@ public:
 	Meter(int n) : value(n) {}
 };
 
-Meter operator""m( int value )
+Meter operator""m( unsigned long long value )
 {
-	Meter meter(value);
+	Meter meter( static_cast<int>(value));
 	return meter;
 }
 
 int main()
 {
-	double d = 3.4; // "3.4"  는 double
-	float f = 3.4f; // "3.4f" 는 float
-
 	auto a1 = 3m;   // 이순간 operator""m(3) 이 호출됩니다.
 //	auto a2 = 3cm;  //       operator""cm(3) 
+
+
+	double d = 3.4; // "3.4"  는 double
+	float f = 3.4f; // "3.4f" 는 float
 }
 
 
