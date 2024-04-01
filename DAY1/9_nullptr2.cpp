33ddca63939f1e153acff8147f953794667e4501
ecourse <smkang@codenuri.co.kr>
2024-04-01 11:42:38 +0900

    Add files via upload

---
+++ DAY1/9_nullptr2.cpp
@@ -0,0 +1,28 @@
+﻿// 8_nullptr2
+#include <iostream>
+
+template<typename F, typename ARG>
+void forward_parameter(F f, ARG arg)
+{
+	f(arg);
+}
+
+void foo(int* p) {}
+
+int main()
+{
+	int n = 0;
+	foo(0); // ok
+//	foo(n);	// error. literal 0은 포인터로 암시적 형변환 되지만 
+			//        0으로 초기화된 변수를 전달할수는 없습니다.
+
+	forward_parameter(foo, 0); // foo(0) 으로 실행해 달라는 의도 ?
+}
+
+
+
+
+
+
+
+
