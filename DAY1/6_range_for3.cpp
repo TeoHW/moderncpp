162bd365f8b0e673cc9c32f168faa17e43d8f6ed
ecourse <smkang@codenuri.co.kr>
2024-04-01 11:00:37 +0900

    Add files via upload

--- DAY1/6_range_for3.cpp
+++ DAY1/6_range_for3.cpp
@@ -1,15 +1,35 @@
 ﻿#include <iostream>
 #include <vector>
 
+template<typename T>
+auto mybegin(T& c) { return c.begin();}
+
+template<typename T>
+auto myend(T& c) { return c.end();}
+
+// 배열 버전
+template<typename T, int N>
+auto mybegin( T(&arr)[N] ) { return arr; } // 배열의 이름은 배열의 주소
+
+template<typename T, int N>
+auto myend( T(&arr)[N] ) { return arr + N; } 
+
+
 struct Point3D
 {
 	int x, y, z;
 
-	int* begin() { return &x;}
-	int* end()   { return &z + 1 ;} // end 는 마지막요소의 다음 주소
+//	int* begin() { return &x;}
+//	int* end()   { return &z + 1 ;} // end 는 마지막요소의 다음 주소
 };
+// 멤버가 아닌 일반함수로 만들어 놓아도 됩니다.
+int* begin(Point3D& p) { return &(p.x);}
+int* end(Point3D& p)   { return &(p.z) + 1 ;}
+
+
 // 단 위 코드는 모든 멤버가 동일한 타입이고, 
 // 연속된 메모리에 놓여 있다는 가정하에만 사용가능합니다.
+
 
 
 int main()
