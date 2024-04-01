968dcd95ed3800f699761269ccdc81bb4992404f
ecourse <smkang@codenuri.co.kr>
2024-04-01 15:39:57 +0900

    Add files via upload

--- DAY1/18_if_init.cpp
+++ DAY1/18_if_init.cpp
@@ -9,9 +9,16 @@ int main()
 	if (ret == 0)
 	{
 	}
+
 	// C++17 부터 위 코드를 아래처럼 사용가능합니다
+	// if ( init-statement; condition-statment )
 	if( int ret2 = foo(); ret2 == 0)
 	{
 	} // <== 여기서 ret2 파괴
 
+	// switch 도 가능
+	switch( int n = foo(); n)
+	{
+		case 1: break;
+	}
 }
