89428020e016aadde85ae3d3d1835f5e6bd285c3
ecourse <smkang@codenuri.co.kr>
2024-04-01 10:56:21 +0900

    Add files via upload

---
+++ DAY1/5_beginend2.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <list>
+#include <iostream>
+
+// 컨테이너 버전
+template<typename T>
+auto begin(T& c) { return c.begin();}
+
+template<typename T>
+auto end(T& c) { return c.end();}
+
+// 배열 버전
+template<typenane T, int N>
+auto begin( T(&arr)[N] ) { return arr; } // 배열의 이름은 배열의 주소
+
+template<typenane T, int N>
+auto end( T(&arr)[N] ) { return arr + N; } 
+
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+	int x[5] = { 1,2,3,4,5 };
+
+	// std::begin() 는 컨테이너와 vector 모두에 사용가능합니다.
+	// => 직접 만들어서 원리를 생각해 봅시다.
+	auto p1 = mybegin(v);
+	auto p2 = mybegin(x);
+
+
+}
