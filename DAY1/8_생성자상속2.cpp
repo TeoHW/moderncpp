2a1077fc3affcce345605eed2c5157248bfd4e33
ecourse <smkang@codenuri.co.kr>
2024-04-01 11:20:46 +0900

    Add files via upload

---
+++ DAY1/8_생성자상속2.cpp
@@ -0,0 +1,24 @@
+﻿#include <list>
+#include <iostream>
+
+class MyList : public std::list<int>
+{
+public:
+	// C++ 표준 std::list 에 함수 한개 정도만 추가하고 싶었다.
+	void dump() const 
+	{
+		for( auto& e : *this )
+			std::cout << e << ", ";
+	}
+};
+
+int main()
+{
+	MyList st1(10, 3); // 10개를 3으로 초기화
+	MyList st2(10);
+	MyList st3 = {1,2,3,4,5};
+}
+
+
+
+
