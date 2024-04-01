9293b6238ebb035cb1ae79596e9365065c583da3
ecourse <smkang@codenuri.co.kr>
2024-04-01 11:23:03 +0900

    Add files via upload

--- DAY1/8_생성자상속2.cpp
+++ DAY1/8_생성자상속2.cpp
@@ -10,6 +10,9 @@ public:
 		for( auto& e : *this )
 			std::cout << e << ", ";
 	}
+
+	// std::list 와 동일하게 사용할수 있도록, 생성자도 상속해 달라.
+	using std::list<int>::list;
 };
 
 int main()
@@ -17,6 +20,8 @@ int main()
 	MyList st1(10, 3); // 10개를 3으로 초기화
 	MyList st2(10);
 	MyList st3 = {1,2,3,4,5};
+
+	std3.dump();
 }
 
 
