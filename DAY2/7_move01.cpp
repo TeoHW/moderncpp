6671d513723417453b6afbaf9a0d9f39d414a250
ecourse <smkang@codenuri.co.kr>
2024-04-02 16:34:23 +0900

    Add files via upload

--- DAY2/7_move01.cpp
+++ DAY2/7_move01.cpp
@@ -9,12 +9,21 @@ int main()
 	std::string s1 = "hello";
 	std::string s2 = "hello";
 
-	std::string s3 = s1;	// 자원을 복사
+	std::string s3 = s1;			// 자원을 복사
 	std::string s4 = std::move(s2);	// 자원을 이동(s2의 자원을 s4가 빼앗는것)
 
 	std::cout << s1 << std::endl; // "hello"
 	std::cout << s2 << std::endl; // ""
-
-
 }
 
+/*
+// RUST
+s1 = String::new("Hello");
+
+s2 = s1;         // 자원의 이동
+s3 = s2.Clone(); // 자원의 복사
+
+foo(s3);
+
+// s3사용 못함.
+*/
