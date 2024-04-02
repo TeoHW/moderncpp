e444a8a24bd496b975fd1e674658e0cfdedfe1a2
ecourse <smkang@codenuri.co.kr>
2024-04-02 10:13:10 +0900

    Add files via upload

---
+++ DAY2/3_초기화5.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+#include <string>
+#include <memory>
+
+void goo(std::vector<int> v) {}
+void hoo(std::string s) {}
+
+int main()
+{
+	hoo("hello"); // ok. 되야 하고, 잘됩니다. std::string s = "hello" 
+	std::string s1("hello"); 	// ok
+	std::string s2 = "hello";	// ok
+								// string(const char*) 는 explicit 아님.
+
+	goo(10); // 안되야 하고, 그래서 에러 발생합니다.
+	std::vector<int> v1(10);  // ok
+	std::vector<int> v2 = 10; // error
+							  // vector(std::size_t) 생성자는 explicit 라는 의미
+	
+
+
+
+
+}
+
+
+
+
+
