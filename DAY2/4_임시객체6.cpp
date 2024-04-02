a8d8815dad7c8a0487fb997a0469db2d9ace3d0b
ecourse <smkang@codenuri.co.kr>
2024-04-02 13:33:22 +0900

    Add files via upload

---
+++ DAY2/4_임시객체6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+// 85 page
+
+struct Base
+{
+	int value = 10; 
+};
+struct Derived : public Base
+{
+	int value = 20; // ok. 기반 클래스 멤버와 이름이 같아도 됩니다.
+};
+int main()
+{
+	Derived d;
+	std::cout << d.value << std::endl;  // 20
+
+	std::cout << static_cast<Base>(d).value << std::endl;  // 10
+	std::cout << static_cast<Base&>(d).value << std::endl;  // 10
+
+}
+
