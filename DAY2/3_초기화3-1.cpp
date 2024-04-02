ba4eeb3cf90bf714c724cdfaca6047bfaa701ef3
ecourse <smkang@codenuri.co.kr>
2024-04-02 10:03:11 +0900

    Add files via upload

--- DAY2/3_초기화3-1.cpp
+++ DAY2/3_초기화3-1.cpp
@@ -7,9 +7,11 @@ public:
 void f1(int n) {}
 void f2(Point pt) {}
 
-Point f3() { return ? }
+Point f3() { return {1,2}; } // 이 표기법도 가능합니다.
 
 int main()
 {
-	f1(3);
+	f1(3);   // int n = 3
+	f1({3}); // int n = {3}
+	f2({1,2}) // Point pt = {1,2}
 }
