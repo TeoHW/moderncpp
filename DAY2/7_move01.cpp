3402343b145cb8ccb8cae28e7bdfcae2a4d5ade4
ecourse <smkang@codenuri.co.kr>
2024-04-02 15:29:50 +0900

    Add files via upload

---
+++ DAY2/7_move01.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+// move 개념
+
+int main()
+{
+	std::string s1 = "hello";
+	std::string s2 = "hello";
+
+	std::string s3 = s1;	// 자원을 복사
+	std::string s4 = std::move(s2);	// 자원을 이동(s2의 자원을 s4가 빼앗는것)
+
+	std::cout << s1 << std::endl; // "hello"
+	std::cout << s2 << std::endl; // ""
+
+
+}
+
