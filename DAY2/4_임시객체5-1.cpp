515304799d9829c017f5a120c765090612f11788
ecourse <smkang@codenuri.co.kr>
2024-04-02 12:11:49 +0900

    Add files via upload

--- DAY2/4_임시객체5-1.cpp
+++ DAY2/4_임시객체5-1.cpp
@@ -4,17 +4,29 @@ class Counter
 {
 	int cnt = 0;
 public:
-	Counter inc() 
+//	Counter inc() 
+	Counter& inc() 
 	{ 
 		++cnt;
 		return *this;
 	}
 	int get() const { return cnt;}
+
+	~Counter() { std::cout << "~Counter()" << std::endl; }
+
+	Counter(const Counter& other) : cnt{other.cnt}
+	{
+		std::cout << "copy ctor" << std::endl;
+	}
+	Counter() = default;
+
 };
 int main()
 {
 	Counter c;
+	std::cout << "-------------------\n";
 	c.inc().inc().inc();
+	std::cout << "-------------------\n";
 
 	std::cout << c.get() << std::endl;
 }
