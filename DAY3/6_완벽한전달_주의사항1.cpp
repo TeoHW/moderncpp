91175c9b2eaf4720dafc33fbbefa7c650c0748ab
ecourse <smkang@codenuri.co.kr>
2024-04-03 13:35:01 +0900

    Add files via upload

---
+++ DAY3/6_완벽한전달_주의사항1.cpp
@@ -0,0 +1,28 @@
+﻿// 9_완벽한전달_주의사항
+#include <iostream>
+
+template<typename F, typename T>
+void chronometry(F f, T&& arg) // int&& arg = 0
+{								// foo(arg) 의 모양인데, arg 는 int 이므로
+	f(std::forward<T>(arg));	// foo(int*) 에는 전달 안됨.
+}
+
+void foo(int* p) {}
+
+int main()
+{
+	foo(0); // ok
+
+//	chronometry(foo, 0); // error
+	chronometry(foo, nullptr); // ok - 1일차 nullptr 예제 참고
+
+}
+
+
+
+
+
+
+
+
+
