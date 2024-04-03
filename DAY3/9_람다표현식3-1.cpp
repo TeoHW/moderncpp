7f1dae0ce19ac61afbef87c7c9cd1600dc72e3e3
ecourse <smkang@codenuri.co.kr>
2024-04-03 16:51:31 +0900

    Add files via upload

--- DAY3/9_람다표현식3-1.cpp
+++ DAY3/9_람다표현식3-1.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
-
 int main()
 {
 	// 다음중 에러는 모두 고르세요
 	auto  f1 = [](int a, int b) { return a + b; };
-	auto& f2 = [](int a, int b) { return a + b; };
-	
-	const auto& f3 = [](int a, int b) { return a + b; };
-	auto&& f4 = [](int a, int b) { return a + b; };
 
+	auto& f2 = [](int a, int b) { return a + b; }; // error
+
+	const auto& f3 = [](int a, int b) { return a + b; }; // ok
+
+	auto&& f4 = [](int a, int b) { return a + b; };
+	// T&& f4 = 함수인자
+
+	// 참고 auto&& 는 "T&&" 와 동일
+	int n = 3;
+	auto&& a1 = n; // ok  auto = int&   auto&&=int& &&  int& a1 = n
+	auto&& a2 = 3; // ok  auto = int    auto&&=int&&    int&& a2 = 3
 }
