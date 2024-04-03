53de2bb48c1a37d483ccbe849d7c9f57d300b87b
ecourse <smkang@codenuri.co.kr>
2024-04-03 17:03:41 +0900

    Add files via upload

--- DAY3/9_람다표현식4-1.cpp
+++ DAY3/9_람다표현식4-1.cpp
@@ -8,7 +8,17 @@ void sort(T pred)
 int main()
 {
 	// 아래 코드를 평가해 보세요.. 좋은 코드일까요 ?
+	// 문제점 : sort() 함수가 3개 생성됩니다.
+	/*
 	sort( [](int a, int b) { return a < b;} );
 	sort( [](int a, int b) { return a < b;} );
 	sort( [](int a, int b) { return a < b;} );
+	*/
+
+	// 동일한 람다 표현식이 여러번 사용되면 되도록 auto 변수에 담아서 사용하세요
+	auto cmp = [](int a, int b) { return a < b;};
+
+	sort( cmp );
+	sort( cmp );
+	sort( cmp );
 }
