6ca2cfa81497bcaa3c6c7c8c0e61b4cbc927ba54
ecourse <smkang@codenuri.co.kr>
2024-04-03 13:48:52 +0900

    Add files via upload

--- DAY3/7_람다표현식소개1.cpp
+++ DAY3/7_람다표현식소개1.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <algorithm>
 
+// 단항 조건자(unary predicate) 
+// => 인자가 한개이고 반환 타입이 bool 또는 bool로 변환가능한 함수
 bool foo(int n) { return n % 3 == 0; }
 
 int main()
@@ -15,3 +17,5 @@ int main()
 //	auto ret1 = std::find_if( v.begin(), v.end(), 함수);
 	auto ret1 = std::find_if( v.begin(), v.end(), foo);
 }
+
+
