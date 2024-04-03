c08f24623dbf09613f317d2ba24d0b3a4f8693f4
ecourse <smkang@codenuri.co.kr>
2024-04-03 13:51:37 +0900

    Add files via upload

--- DAY3/7_람다표현식소개1.cpp
+++ DAY3/7_람다표현식소개1.cpp
@@ -16,6 +16,22 @@ int main()
 	// 주어진 구간에서 3의 배수를 찾고 싶다.
 //	auto ret1 = std::find_if( v.begin(), v.end(), 함수);
 	auto ret1 = std::find_if( v.begin(), v.end(), foo);
+
+
+	// 람다 표현식(lambda expression)
+	// => 함수 이름(주소)가 필요한 자리에 함수 구현을 직접 표기하는 문법
+	// => 익명의 함수를 만드는 문법
+
+	// [] : 람다표현식이 시작됨을 알리는 기호(lambda introducer )
+
+	auto ret2 = std::find_if( v.begin(), v.end(), 
+							[](int n) { return n % 3 == 0; } );
 }
+
+
+
+
+
+
 
 
