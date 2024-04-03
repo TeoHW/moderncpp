f71e787f96211d20a415a629382af495d38dd4f4
ecourse <smkang@codenuri.co.kr>
2024-04-03 13:57:42 +0900

    Add files via upload

--- DAY3/7_람다표현식소개2.cpp
+++ DAY3/7_람다표현식소개2.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <algorithm>
 
-// 단항 조건자(unary predicate) 
-// => 인자가 한개이고 반환 타입이 bool 또는 bool로 변환가능한 함수
 bool foo(int n) { return n % 3 == 0; }
 
 int main()
@@ -13,13 +11,17 @@ int main()
 	int k = 3; // 사용자가 입력한 값이라고 가정
 
 	// 주어진 구간에서 k의 배수를 찾고 싶다 ???!
+	// => 일반 함수인 foo 안에서는 지역변수 k에 접근할수 없습니다.!!
+	// => 일반 함수로는 이문제 해결안됩니다(전역변수등을 사용해야만 합니다.)
+//	auto ret1 = std::find_if( v.begin(), v.end(), foo);
 
-	auto ret1 = std::find_if( v.begin(), v.end(), foo);
 
+	// 람다표현식은 지역변수를 캡쳐 할수 있는 능력이 있습니다.
 	auto ret2 = std::find_if( v.begin(), v.end(), 
-							[](int n) { return n % 3 == 0; } );
+							[k](int n) { return n % k == 0; } );
+							// "단항함수 + 지역변수 k" 를 인자로 보내는 의미
 }
-
+// 람다 표현식 장점 1. 지역변수 캡쳐
 
 
 
