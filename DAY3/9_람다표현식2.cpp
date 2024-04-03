62aa6b6d7464b10288db9cbf67b897c2d821923f
ecourse <smkang@codenuri.co.kr>
2024-04-03 16:29:35 +0900

    Add files via upload

---
+++ DAY3/9_람다표현식2.cpp
@@ -0,0 +1,29 @@
+﻿// 5_람다표현식1
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+
+	// 람다표현식의 원리(lambda expression)
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+
+	//--------------------------------------------------------------
+	class CompilerGeneratedName 
+	{
+	public:
+		inline auto operator()(int a, int b) const 
+		{ 
+			return a < b; 
+		}
+	};
+	std::sort(v.begin(), v.end(), CompilerGeneratedName{} );
+
+}
+
+
+
+
+
