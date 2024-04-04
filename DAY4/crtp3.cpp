c026e5affaa098ccb39cb664fbfc034693f1056c
ecourse <smkang@codenuri.co.kr>
2024-04-04 14:10:20 +0900

    Add files via upload

---
+++ DAY4/crtp3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template<typename T>
+class Window
+{
+public:
+	void event_loop() 	// void event_loop(Window* this)
+	{
+//		Click();		// this->Click();
+		static_cast<T*>(this)->Click();
+	}
+	void Click()  { std::cout << "Window Click" << std::endl; }
+};
+
+class MainWindow : public Window< MainWindow >
+{
+public:
+	void Click()  { std::cout << "MainWindow Click" << std::endl; }
+};
+
+int main()
+{
+	MainWindow w;
+	w.event_loop();
+}
