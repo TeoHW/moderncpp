8d18e6de876810dcd6e1f00cb45a39fee7d46cca
ecourse <smkang@codenuri.co.kr>
2024-04-04 15:07:12 +0900

    Add files via upload

--- DAY4/ctor5.cpp
+++ DAY4/ctor5.cpp
@@ -4,6 +4,11 @@ class Base
 {
 public:
 	Base()     { vfunc(); } // Base::foo
+							// 이 코드가 Derived foo 를 호출했다면 화면 출력결과 예측해보세요
+							// => Derived::vfunc 에서 사용하는 data 는 초기화되기 전입니다.!!
+							// => 이런 문제 때문에
+							// "생성자에서는 가상함수가 동작하지 않는다" 라는 문법이 있습니다.
+
 	void foo() { vfunc(); } // Derived::foo
 	virtual void vfunc() { std::cout << "Base vfunc" << std::endl; }
 };
