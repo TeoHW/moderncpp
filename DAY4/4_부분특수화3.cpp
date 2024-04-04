c915c81584ac430b4f2711e5c7e1c87fa3487d50
ecourse <smkang@codenuri.co.kr>
2024-04-04 15:54:41 +0900

    Add files via upload

--- DAY4/4_부분특수화3.cpp
+++ DAY4/4_부분특수화3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // C++98 시절에 만든 표준 less
-template<typename T>
+template<typename T = void >
 struct less 
 {
 	inline bool operator()(T a, T b) const 
@@ -10,8 +10,26 @@ struct less
 	}
 };
 
+// C++14에서 추가한 less
+template<>
+struct less<void>
+{
+	template<typename T1, typename T2>
+	inline bool operator()(T1 a, T2 b) const 
+	{
+		return a < b;
+	}
+};
+
 int main()
 {
 	less<int> f;
-	auto f = f(3, 3.2);
+	bool ret = f(3, 3.2); // ??
+
+	less<int> f1; // int 끼리만 비교 가능
+	less<void> f2; // 서로 다른 타입 비교 가능
+	less<> f3; // 위와 동일
+	less f4; // 이렇게도 가능
+
+	std::sort(v.begin(), v.end(), std::less{});
 }
