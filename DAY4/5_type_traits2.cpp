55e43b265c705c7f809f0512ed25130329b5f8a7
ecourse <smkang@codenuri.co.kr>
2024-04-04 15:58:17 +0900

    Add files via upload

---
+++ DAY4/5_type_traits2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <type_traits>
+
+template<typename T> struct is_pointer
+{
+	enum { value = false };
+};
+
+template<typename T> void foo(const T& a)
+{
+	if ( is_pointer<T>::value )
+		std::cout << "포인터" << std::endl;
+	else
+		std::cout << "포인터 아님" << std::endl;
+}
+
+
+
+int main()
+{
+	int n = 0;
+	foo(n);
+	foo(&n);
+}
