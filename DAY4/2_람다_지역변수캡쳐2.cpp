d29cb587c381527c28d569de3ad20c8ba2faddd2
ecourse <smkang@codenuri.co.kr>
2024-04-04 09:14:21 +0900

    Add files via upload

--- DAY4/2_람다_지역변수캡쳐2.cpp
+++ DAY4/2_람다_지역변수캡쳐2.cpp
@@ -7,9 +7,12 @@ int main()
 {
 	int v1 = 0, v2 = 0;
 
-	auto f2 = [v1, v2](int a) { v1 = a; v2 = a; }; 
+//	auto f2 = [v1, v2](int a) { v1 = a; v2 = a; }; 
+	auto f2 = [v1, v2](int a) mutable { v1 = a; v2 = a; }; 
 
 	f2(100);
+
+	std::cout << v1 << ", " << v2 << std::endl; // 0, 0
 
 	// 람다 지역변수 캡쳐의 원리
 	// => 멤버 데이타를 생성하는 것
@@ -19,14 +22,14 @@ int main()
 		int v2;
 	public:
 		CompilerGeneratedName(int a, int b) : v1{a}, v2{b} {}
-
-		inline auto operator()(int a) const 
+//		inline auto operator()(int a) const // non-mutable lambda
+		inline auto operator()(int a)    	// mutable lambda
 		{ 
 			v1 = a; 
 			v2 = a; 
 		}
 	};
-	auto f2 = CompilerGeneratedName{v1, v2}; // <= 여기 v1, v2는
+	auto f3 = CompilerGeneratedName{v1, v2}; // <= 여기 v1, v2는
 											 // main 의 지역변수
 }
 
