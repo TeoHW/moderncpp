66becdd4b0a9ce86c1a0214ae3ee199eb7818b56
ecourse <smkang@codenuri.co.kr>
2024-04-04 13:39:52 +0900

    Add files via upload

--- DAY4/3_람다기타3.cpp
+++ DAY4/3_람다기타3.cpp
@@ -5,10 +5,18 @@ class ClosureType
 public:
 	inline int operator()(int a, int b) const { return a + b; }
 
+	// 람다표현식 사용시 함수 포인터로 변환하는 코드를 사용하면
+	// 아래 와 같은 도움함수가 추가
+	inline static int helper(int a, int b)  
+	{ 
+		return ClosureType::operator()(a, b);
+	}
+
+
 	using FP = int(*)(int, init);
 
 	// 변환연산자 함수 : 객체가 다른 타입으로 변환이 필요 할때 호출
-	operator FP() { return 함수주소; }
+	operator FP() { return &ClosureType::helper ; }
 };
 
 int main()
