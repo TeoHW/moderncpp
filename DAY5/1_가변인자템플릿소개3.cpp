b9c70371cb92c62fc7312e5ce6025913d2935b84
ecourse <smkang@codenuri.co.kr>
2024-04-05 08:52:05 +0900

    Add files via upload

--- DAY5/1_가변인자템플릿소개3.cpp
+++ DAY5/1_가변인자템플릿소개3.cpp
@@ -5,12 +5,18 @@
 //     2. 1번째 인자로 반드시 나머지 인자의 갯수를 보내야 합니다.
 int sum( int cnt, ... )
 {
+	int s = 0;
+
 	va_list ap;			// 대부분의 컴파일러에서 char* ap 입니다.
 	va_start(ap, cnt);	// ap = cnt 의 다음 인자를 가리키도록 설정
 
-	int n = va_arg(ap, int); // ap 에서 int 를 꺼내고, ap 를 int 크기 만큼
-	std::cout << n << std::endl;
-	return 0;
+	for ( int i = 0; i < cnt; i++)
+	{
+		s += va_arg(ap, int); // ap 에서 int 를 꺼내고, ap 를 int 크기 만큼
+	}
+	
+	va_end(ap); // 특별하게 하는 일은 없지만, 표준에서는 꼭 이렇게 하라고 되어있습니다.
+	return s;	
 }
 
 int main()
