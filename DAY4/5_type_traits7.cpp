a323b5b1e175d1c8aef5a31dd2fb89e2732fd43f
ecourse <smkang@codenuri.co.kr>
2024-04-04 16:42:05 +0900

    Add files via upload

--- DAY4/5_type_traits7.cpp
+++ DAY4/5_type_traits7.cpp
@@ -9,11 +9,14 @@ public:
 	Object(Object&&) noexcept {	std::cout << "move" << std::endl; }
 };
 
+// 아래 코드는 임시객체(rvalue)를 받을수 없는 잘못된 구현
+
 template<typename T>
 T&& mymove( T& obj)
 {
 	return static_cast<T&&>(obj);
 }
+
 
 int main()
 {
@@ -21,4 +24,5 @@ int main()
 	Object o2 = o1;				// copy
 	Object o3 = std::move(o2); 	// move
 	Object o4 = mymove(o3); 	// move
+	Object o5 = mymove( Object{} ); 	// error
 }
