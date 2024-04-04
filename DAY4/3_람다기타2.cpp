61d1d25b22c4e61f0e24dff4f4f609195d356733
ecourse <smkang@codenuri.co.kr>
2024-04-04 10:09:18 +0900

    Add files via upload

---
+++ DAY4/3_람다기타2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+// C 언어로 만들었던 함수.
+// => 인자로 함수 포인터를 받는 함수.
+void register_callback( void(*f)(int, int) )
+{
+	f(1, 2);
+	f(1, 2);
+	f(1, 2);
+}
+
+void foo(int a, int b) 
+{
+	std::cout << "foo\n";
+}
+int main()
+{
+	register_callback(&foo);
+
+	register_callback( [](int a, int b) { std::cout << "lambda\n"; } );
+}
