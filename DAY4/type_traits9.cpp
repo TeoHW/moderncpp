057cd4a8d65bce833367a1bb8a9949e29a2a442b
ecourse <smkang@codenuri.co.kr>
2024-04-04 17:11:10 +0900

    Add files via upload

--- DAY4/type_traits9.cpp
+++ DAY4/type_traits9.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <type_traits>
 #include <concepts>
+#include <vector>
+#include <algorithm>
 
 // 값 버전과 조건자 버전의 find 를 같은 이름을 사용할수 없습니다.
 // 아래 코드를 생각해보면 모양이 완전히 동일해 집니다.
@@ -32,4 +34,13 @@ int main()
 	find(x, x+3, 2);
 
 	find(x, x+3, [](int n){ return n %2 == 0;});
+
+
+//	std::sort(v.begin(), v.end()); // 가 아니라
+//	std::sort(v);				// 왜 이게 안될까 ?
+
+	std::vector<int> v{1,2,3,4,5};
+
+	std::ranges::sort(v.begin(), v.end()); // C++20 부터이렇게 해도되고
+	std::ranges::sort(v); // 이렇게 해도 됩니다.
 }
