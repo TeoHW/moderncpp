6cf91069f7fd2bc3a98f6fcb8ed993c127d79eb0
ecourse <smkang@codenuri.co.kr>
2024-04-05 15:30:29 +0900

    Add files via upload

--- DAY5/projection1.cpp
+++ DAY5/projection1.cpp
@@ -12,7 +12,17 @@ int main()
 	std::string s1 = "AAAA";
 	std::string s2 = "XX";
 
-	auto ret = Max(s1, s2);
+	std::cout<< Max(s1, s2) << std::endl; // "XX"
 
-	std::cout << ret << std::endl;
+	// s1, s2 중에서 문자열의 "길이가 큰" 문자열을 얻고 싶다.
+	auto ret1 = Max( s1.size(), s2.size()  ); // ret1 의 타입은 int
+
+	// 아래 2가지 사용법중 좋은 것을 골라 보세요
+
+	// #1. 이항 함수를 사용
+	auto ret1 = Max( s1, s2, 
+				[](const std::string& a, const std::string& b) { return a.size() < b.size();}
+
+	// #2. 단항 함수를 사용
+	auto ret2 = Max( s1, s2, [](const std::string& s) {return s.size();});
 }
