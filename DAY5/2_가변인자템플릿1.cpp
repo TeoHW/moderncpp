d545520cd3c340aa23325ebf75d08aba1ed8be5c
ecourse <smkang@codenuri.co.kr>
2024-04-05 09:03:50 +0900

    Add files via upload

---
+++ DAY5/2_가변인자템플릿1.cpp
@@ -0,0 +1,15 @@
+﻿// 7_가변인자템플릿1 - 225 page
+
+// 인자가 한개인 함수 템플릿 : 인자가 한개인 함수를 생성하는 도구
+// 가변인자 함수 템플릿      : 임의 갯수의 인자를 가지는 함수를 생성하는 도구
+// 가변인자 함수 템플릿
+template<typename ... Ts> void foo(Ts ... args) {}
+								
+int main()
+{
+	foo();
+	foo(1);
+	foo(1, 3.4);
+	foo(1, 3.4, "A");  	// Ts   : int, double, const char*
+						// args : 1,   3.4,    "A"
+}
