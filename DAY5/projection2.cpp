ae72a7d4fd8f99dcf73156de15f140da0933e481
ecourse <smkang@codenuri.co.kr>
2024-04-05 15:38:01 +0900

    Add files via upload

--- DAY5/projection2.cpp
+++ DAY5/projection2.cpp
@@ -9,5 +9,13 @@ int main()
 	std::string s2 = "XX";
 
 	auto ret1 = std::ranges::max(s1, s2); // "XX"
-	auto ret2 = std::ranges::max(s1, s2, std::less{}, [](const std::string& s){ return s.size();}); 
+
+
+	auto ret2 = std::ranges::max(s1, s2, std::less{}, 
+
+					[](const std::string& s){ return s.size();}); 
+
+	auto ret3 = std::ranges::max(s1, s2, std::less{}, &std::string::size ); 					
+
+	auto ret4 = std::ranges::max(s1, s2, {}, &std::string::size ); 					
 }
