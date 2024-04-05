82bc0b389d06c5bef0593cb34b0055fc9f42a66a
ecourse <smkang@codenuri.co.kr>
2024-04-05 10:47:03 +0900

    Add files via upload

---
+++ DAY5/4_tuple2.cpp
@@ -0,0 +1,16 @@
+﻿// Step1. 가변인자 템플릿 사용
+
+template<typename ... Ts>
+struct tuple 
+{
+
+	static constexpr std::size_t N = 0; // 0개 보관	
+};
+
+int main()
+{
+	tuple<> t0;
+	tuple<short> t1;
+	tuple<double, short> t2;
+	tuple<int, double, short> t3;
+}
