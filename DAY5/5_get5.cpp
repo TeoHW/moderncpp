bd0ffff3bf8d94a966cac65ef7707e5a679a8e6e
ecourse <smkang@codenuri.co.kr>
2024-04-05 12:02:35 +0900

    Add files via upload

--- DAY5/5_get5.cpp
+++ DAY5/5_get5.cpp
@@ -48,10 +48,22 @@ int main()
 {
 	tuple<int, double, short> t(1, 3.4, 2);
 
-	auto a = get<1>(t); // 결국 "t.second_member_name" 같이
+	auto a = get<1>(t); // 결국 "t.second_member_name" 같이 (pt.y)
 					    // 구조체의 필드를 이름으로 접근하는 것과 
 						// 완전히 동일한 기계어 코드가 됩니다.
 	get<0>(t) = 20;
 
 	std::cout << get<0>(t) << std::endl;
+
+	// get<i>(t) 는 안되나요 ?? :  안됩니다. literal 만 가능
+
+	// 구조체 필드 접근시 반드시 이름으로만 해야 합니다.
+	// pt.x
+	// pt.y
+	// => 구조체 각  필드를 변수인덱스로 접근 할수 없습니다.
+
+	// get<0>(t), get<1>(t) 에서 0, 1 을 필드 이름으로 생각하세요
+	// 따라서, 변수 i 는 사용할수 없습니다.
 }
+
+//pt.y =10; // *(&pt + y의offset(4)) = 10
