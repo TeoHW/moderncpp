6dfaf6547ee6069bf2d9b58ca229e35e9df2285c
ecourse <smkang@codenuri.co.kr>
2024-04-05 15:06:32 +0900

    Add files via upload

--- DAY5/qa1.cpp
+++ DAY5/qa1.cpp
@@ -6,9 +6,11 @@ int main()
 	std::string s1 = "hello";
 	std::string s2 = "hello";
 
-
-	auto f1 = [other_name = std::move(s1)]() { };
-//	auto f2 = [s1 = std::move(s2) ]() { };
+	auto f1 = [other_name = std::move(s1)]() { }; // ok
+//	auto f2 = [s2 = std::move(s2) ]() { };		  // ok
+//	auto f3 = [std::move(s2) ]() { };		  // error
+	auto f3 = [move(s2) ]() { };		  // ok.
+	//        [move = s2]() { move 라는 변수이름 사용}
 
 	std::cout << s1 << std::endl; // ""
 	std::cout << s2 << std::endl;
