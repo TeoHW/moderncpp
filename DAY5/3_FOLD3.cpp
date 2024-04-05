17c8f7a5cb04ba580ff84053a2a1f1e7886f4511
ecourse <smkang@codenuri.co.kr>
2024-04-05 10:23:51 +0900

    Add files via upload

--- DAY5/3_FOLD3.cpp
+++ DAY5/3_FOLD3.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
 
+// 구글에서 "C++ fold example" 하시면 아주 많은 예제가 있습니다.
+
 template<typename ... Ts>
 void add_vector(std::vector<int>& v, Ts ... args)
 {
 	// args 의 모든 요소를 v에 넣에 보세요
+	( v.push_back(args) , ... );
 }
 int main()
 {
