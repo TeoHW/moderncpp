cc58fba92e2500acc8d6c10cad799ffa2a44e297
ecourse <smkang@codenuri.co.kr>
2024-04-05 09:06:36 +0900

    Add files via upload

---
+++ DAY5/2_가변인자템플릿2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+template<typename ... Types> void foo(Types ... args)
+{
+	// Types : int, int, int   => template parameter pack
+	// args  : 1,   2,   3     => function parameter pack
+
+	// parameter pack 안에 몇개가 있는지 조사하는 방법
+	std::cout << sizeof...(Types) << std::endl;
+	std::cout << sizeof...(args)  << std::endl;
+}
+
+int main()
+{
+	foo(1, 2, 3);
+}
