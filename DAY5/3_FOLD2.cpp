2817043b60ae0baf3908f1eb08324de0be92139c
ecourse <smkang@codenuri.co.kr>
2024-04-05 10:18:55 +0900

    Add files via upload

---
+++ DAY5/3_FOLD2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+template<typename T> void printv(const T& a)
+{
+	std::cout << a << std::endl;
+}
+
+template<typename ... Types>
+void Show(Types ... args)
+{
+	// I        op ... op pack
+//	( std::cout << ... << args  );
+	// ((((std::cout << e1) << e2 ) << e3 )
+
+	// pack 의 이름뿐 아니라 pack 을 사용하는 패턴도 가능
+	(printv(args), ... );
+}
+
+int main()
+{
+	Show(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+}
+
+
+
